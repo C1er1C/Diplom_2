95d03da10d7cdcb3338d1a3f6368f44edc6352c7
Sergey Denisov <ytrewq7070@yandex.ru>
2023-08-15 13:54:05 +0300

    Доделал тесты для заказа.

--- src/test/java/CreateOrderTest.java
+++ src/test/java/CreateOrderTest.java
@@ -1,2 +1,66 @@
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import order.Order;
+import order.OrderDataGenerator;
+import order.OrderSteps;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import user.User;
+import user.UserDataGenerator;
+import user.UserSteps;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class CreateOrderTest {
+    private User user;
+    private String accessToken;
+    private Order order;
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = OrderSteps.baseURL;
+        user = UserDataGenerator.getRandomUser();
+        accessToken = UserSteps.createNewUser(user).then().extract().path("accessToken");
+    }
+    @Test
+    @DisplayName("Создание заказа с авторизацией, с ингредиентами")
+    public void createOrderWithAuthorization() {
+        order = OrderDataGenerator.getOrderData();
+        Response response = OrderSteps.createNewOrderWithAuthorization(accessToken, order);
+        response.then().assertThat().statusCode(200)
+                .and()
+                .body("success", is(true));
+    }
+    @Test
+    @DisplayName("Создание заказа без авторизации, с ингредиентами")
+    public void createOrderWithoutAuthorization() {
+        order = OrderDataGenerator.getOrderData();
+        Response response = OrderSteps.createNewOrderNoAuthorization(order);
+        response.then().assertThat().statusCode(200)
+                .and()
+                .body("success", is(true));
+    }
+    @Test
+    @DisplayName("Создание заказа без ингредиентов")
+    public void createOrderWithoutIngredients() {
+        Response response = OrderSteps.createNewOrderNoIngredients(accessToken);
+        response.then().assertThat().statusCode(400)
+                .and()
+                .body("success", is(false))
+                .and()
+                .body("message", equalTo("Ingredient ids must be provided"));
+    }
+    @Test
+    @DisplayName("Создание заказа с неверным хешем ингредиентов")
+    public void createOrderWithWrongHash() {
+        order = OrderDataGenerator.getInvalidOrderData();
+        Response response = OrderSteps.createNewOrderNoAuthorization(order);
+        response.then().assertThat().statusCode(500);
+    }
+    @After
+    public void cleanUp(){
+        UserSteps.deleteUser(accessToken);
+    }
 }
--- src/test/java/UserOrderTest.java
+++ src/test/java/UserOrderTest.java
@@ -1,2 +1,50 @@
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import order.Order;
+import order.OrderDataGenerator;
+import order.OrderSteps;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import user.User;
+import user.UserDataGenerator;
+import user.UserSteps;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class UserOrderTest {
+    private User user;
+    private String accessToken;
+    private Order order;
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = OrderSteps.baseURL;
+        user = UserDataGenerator.getRandomUser();
+        accessToken = UserSteps.createNewUser(user).then().extract().path("accessToken");
+        order = OrderDataGenerator.getOrderData();
+        OrderSteps.createNewOrderNoAuthorization(order);
+    }
+    @Test
+    @DisplayName("Получение заказов конкретного авторизованного пользователя")
+    public void getUserOrderWithAuthorization() {
+        Response response = OrderSteps.getSomeOrders(accessToken);
+        response.then().assertThat().statusCode(200)
+                .and()
+                .body("success", is(true));
+    }
+    @Test
+    @DisplayName("Получение заказов конкретного неавторизованного пользователя")
+    public void getUserOrderWithoutAuthorization() {
+        Response response = OrderSteps.getSomeOrders("");
+        response.then().assertThat().statusCode(401)
+                .and()
+                .body("success", is(false))
+                .and()
+                .body("message", equalTo("You should be authorised"));
+    }
+    @After
+    public void cleanUp(){
+        UserSteps.deleteUser(accessToken);
+    }
 }
